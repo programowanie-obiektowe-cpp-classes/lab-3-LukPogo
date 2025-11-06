79e252e781fdc62eb5b49cfde2e8499a0b344a22
Lukasz <lukaszpogo321@gmail.com>
2025-11-06 15:37:44 +0100

    Added transportujFlota() function

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,22 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int przetransportowany_towar{};
+    unsigned int liczba_zaglowcow{};
+
+    Stocznia stocznia{};
+
+    while (przetransportowany_towar <= towar)
+    {
+        Statek* s = stocznia();
+
+        if (dynamic_cast<Zaglowiec*>(s))
+            liczba_zaglowcow++;
+
+        przetransportowany_towar += s->transportuj();
+
+        delete s;
+    }
+
+    return liczba_zaglowcow;
 }
